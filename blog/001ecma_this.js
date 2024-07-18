2202dc8f843ed8eb8b0c9b6fd5ace1b958e157e5
elicxh <elicxh@gmail.com>
2024-07-18 16:00:36 +0800

    feat: add blog 001;

---
+++ blog/001ecma_this.js
@@ -0,0 +1,106 @@
+function abc() {
+  console.log(this);
+}
+
+/** 
+ * Runtime Semantics: InstantiateOrdinaryFunctionObject
+ * F = OrdinaryFunctionCreate()
+ *  thisMode = GLOBAL
+ */
+
+/**
+ * this;
+ *   envRec = GetThisEnvironment();
+ *   return envRec.GetThisBinding();
+ */
+
+abc() 
+
+/**
+ * abc 
+ *  ResolveBinding(StringValue of Identifier).
+ *  env = runningExecutionContext.LexicalEnvironment
+ *  return GetIdentifierReference(env, 'abc', strict).
+ *       ReferenceRecord { [[Base]]: env, [[ReferenceName]]: 'abc', [[ThisValue]]: EMPTY }
+ */
+
+
+/**
+ * Situation abc()
+ * 
+ * return EvaluateCall(func, abc, Arguments, tailCall)
+ *    ref is a Reference Record
+ *        ref is a propertyReference
+ *            thisValue = GetThisValue(abc)
+ *        ref is not a propertyReference 
+ *            thisValue = abc.[[Base]].WithBaseObject(); <- undefined
+ *    Call(func, thisValue, argList)
+ * => func.[[call]](thisValue, argList)
+ *        calleeContext = PrepareForOrdinaryCall(F, undefined)
+ *        OrdinaryCallBindThis(F, calleeContext, thisValue)
+ *            thisValue = globalEnv.[[GlobalThisValue]] or ToOBject(thisValue);
+ *            localEnv = calleeContext.LexicalEnvironment
+ *            localEnv.BindThisValue(thisValue);
+ */
+
+/**
+ * Situation obj.abc();
+ * 
+ * obj.abc
+ *    baseReference = obj;
+ *    baseValue = GetValue(baseReference);
+ *    EvaluatePropertyAccessWithIdentifierKey(baseValue, abc, strict);
+ *      return { [[Base]]: baseValue, [[ReferencedName]]: 'abc', [[ThisValue]]: EMPTY }
+ * 
+ * return EvaluateCall(func, obj.abc, Arguments, tailCall)
+ *    ref is a Reference Record
+ *        ref is a propertyReference
+ *            thisValue = GetThisValue(abc)  <= obj
+ *        ref is not a propertyReference 
+ *            thisValue = abc.[[Base]].WithBaseObject(); <- undefined
+ *    Call(func, thisValue, argList)
+ * => func.[[call]](thisValue, argList)
+ *        calleeContext = PrepareForOrdinaryCall(F, undefined)
+ *        OrdinaryCallBindThis(F, calleeContext, thisValue)
+ *            thisValue = globalEnv.[[GlobalThisValue]] or ToOBject(thisValue);
+ *            localEnv = calleeContext.LexicalEnvironment
+ *            localEnv.BindThisValue(thisValue);
+ *                F.[[ThisValue]] =  thisValue
+ *                F.[[ThisBindingStatus]] =  thisValue
+ *                return F;
+ */
+
+/**
+ * const arrowFunc = () => { console.log(this) }
+ *  let closure = OrdinaryFunctionCreate(%Function.prototype%, sourceText, ArrowParameters, ConciseBody, LEXICAL-THIS, env, privateEnv)
+ *      F.[[ThisMode]] = LEXICAL;
+ * 
+ * 
+ * 
+ * Situation arrowFunc():
+ * 
+ * return EvaluateCall(arrow, arrowFunc, arguments)
+ *    ref is a ReferenceRecord
+ *        ref is not PropertyReference
+ *            thisValue = ref.[[Base]].WithBaseObject()
+ *    Call(func, thisValue, argList)
+ * => F.[[Call]](thisValue, argList)
+ *        calleeContext = PrepareForOrdinarycall(F, thisValue);
+ *        OrdinaryCallBindThis(F, calleeContext, thisValue);
+ *            return UNUSED;
+ */
+
+outerEnv
+
+const c = () => console.log(this)
+
+function tt() {
+  function abc() {
+    c()
+  }
+  let obj = {
+    abc: abc
+  }
+  obj.abc();
+}
+tt()
